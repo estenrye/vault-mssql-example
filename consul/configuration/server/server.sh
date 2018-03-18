844c4159a329bf9c5fe55968a78640e2912c30ed
Esten Rye <esten@ryezone.net>
2018-03-17 23:17:42 -0500

    add logic to create data directory.

--- consul/configuration/server/server.sh
+++ consul/configuration/server/server.sh
@@ -5,3 +5,4 @@ sed "s/<<REGION>>/$region/g" /app/server.config.tmpl |
 sed "s/<<MANAGER_COUNT>>/$manager_count/g" |
 sed "s/<<ENCRYPTION_TOKEN>>/$encryption_token/g" > /out/config.json
 cat /out/config.json
+mkdir data
