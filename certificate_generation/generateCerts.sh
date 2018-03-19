195b16abaf37af36b96f98605e35310f7c10e0b6
Esten Rye <esten@ryezone.net>
2018-03-18 20:20:47 -0500

    Working TLS configuration and certificate generation for consul. Still need to find a way for traefik to trust it though.

--- certificate_generation/generateCerts.sh
+++ certificate_generation/generateCerts.sh
@@ -8,74 +8,17 @@ store_secret() {
     fi
 }
 
-mkdir -p /ca/root/certs
-mkdir -p /ca/root/crl
-mkdir -p /ca/root/csr
-mkdir -p /ca/root/newcerts
-mkdir -p /ca/root/private
-mkdir -p /ca/intermediate/certs
-mkdir -p /ca/intermediate/crl
-mkdir -p /ca/intermediate/newcerts
-mkdir -p /ca/intermediate/private
-touch /ca/root/index.txt
-touch /ca/intermediate/index.txt
-echo 1000 > /ca/root/serial
-echo 1000 > /ca/intermediate/serial
-echo 1000 > /ca/root/crlnumber
-echo 1000 > /ca/intermediate/crlnumber
-mkdir -p /out/consul
+mkdir /etc/consul.d/ssl
+mkdir /etc/consul.d/ssl/CA
+chmod 0700 /etc/consul.d/ssl/CA
+cd /etc/consul.d/ssl/CA
+echo "000a" > serial
+touch certindex
 
-region=$(echo $REGION | sed 's/\//\\\//g')
-sed "s/<<REGION>>/$region/g" /ca/intermediate/csr/consul.csr.cnf.tmpl > /ca/intermediate/csr/consul.csr.cnf
+openssl req -x509 -subj '/C=US/ST=MN/L=Minneapolis/O=ConsulCorp/CN=ConsulCA/emailAddress=admin@dev.null.com' -newkey rsa:2048 -days 3650 -nodes -out ca.cert -keyout privkey.pem
+openssl req -subj "/C=US/ST=MN/L=Minneapolis/O=ConsulCorp/CN=*.$TLD/emailAddress=admin@dev.null.com" -newkey rsa:1024 -nodes -out consul.csr -keyout consul.key
+openssl ca -batch -config myca.conf -notext -in consul.csr -out consul.cert
 
-
-# Generate Root CA Private Key
-openssl genrsa -out /ca/root/private/ca.key.pem 4096
-
-# Generate the Root Certificate Authority Certificate
-openssl req -config /ca/root/openssl.cnf \
-	-subj '/C=US/ST=Minnesota/L=Minneapolis/O=Generic CA/OU=Generic Root Certificate Authority/CN=Generic Root CA/emailAddress=no-reply@doesnotexist.com' \
-	-key /ca/root/private/ca.key.pem \
-	-new -x509 -days 7300 -sha256 -extensions v3_ca \
-	-out /ca/root/certs/ca.cert.pem
-
-# Generate the Intermediate Certificate Authority private key.
-openssl genrsa -out /ca/intermediate/private/intermediate.key.pem 4096
-
-# Use the Intermediate Certificate Authority private key to generate a CSR
-openssl req -config /ca/intermediate/openssl.cnf -new -sha256 \
-	-subj '/C=US/ST=Minnesota/L=Minneapolis/O=Generic CA/OU=Generic Intermediate Certificate Authority/CN=Generic Intermediate CA/emailAddress=no-reply@doesnotexist.com' \
-	-key /ca/intermediate/private/intermediate.key.pem \
-	-out /ca/intermediate/csr/intermediate.csr.pem
-
-# Sign the intermediate certificate authority's CSR with the Root Certificate Authority's certificate
-openssl ca -config /ca/root/openssl.cnf -extensions v3_intermediate_ca \
-	-batch -days 3650 -notext -md sha256 \
-	-in /ca/intermediate/csr/intermediate.csr.pem \
-	-out /ca/intermediate/certs/intermediate.cert.pem
-
-# Create the certificate chain file.
-cat /ca/intermediate/certs/intermediate.cert.pem | cat /ca/root/certs/ca.cert.pem > /out/ca-chain.cert.pem
-
-store_secret ca-chain.cert.pem /out/ca-chain.cert.pem
-
-# Generate a private key for Consul.
-openssl genrsa -out /ca/intermediate/private/consul.key.pem 2048
-
-# Generate a CSR for Consul
-openssl req -config /ca/intermediate/csr/consul.csr.cnf -new -sha256  \
-	-subj "/C=US/ST=Minnesota/L=Minneapolis/O=Hashcorp/OU=consul/CN=server.$REGION.consul/emailAddress=no-reply@doesnotexist.com" \
-	-key /ca/intermediate/private/consul.key.pem \
-	-out /ca/intermediate/csr/consul.csr.pem
-
-# Generate a Certificate for Consul
-openssl ca -config /ca/intermediate/openssl.cnf \
-	-batch -extensions server_cert -days 3650 -notext -md sha256 \
-	-in /ca/intermediate/csr/consul.csr.pem \
-	-out /ca/intermediate/certs/consul.cert.pem
-
-cp /ca/intermediate/private/consul.key.pem /out/consul/consul.key.pem
-cp /ca/intermediate/certs/consul.cert.pem /out/consul/consul.cert.pem
-
-store_secret consul.key.pem /out/consul/consul.key.pem
-store_secret consul.cert.pem /out/consul/consul.cert.pem
+store_secret ca.cert.pem ca.cert
+store_secret consul.cert.pem consul.cert
+store_secret consul.key.pem consul.key
--- consul/configuration/configure.sh
+++ consul/configuration/configure.sh
@@ -11,13 +11,16 @@ store_secret() {
 mkdir -p /out
 region=$(echo $REGION | sed 's/\//\\\//g')
 manager_count=$(echo $MANAGER_COUNT | sed 's/\//\\\//g')
+top_level_domain=$(echo $TLD | sed 's/\//\\\//g')
 encryption_token=$(echo $ENCRYPTION_TOKEN | sed 's/\//\\\//g')
 
 sed "s/<<REGION>>/$region/g" /app/server.config.tmpl |
 sed "s/<<MANAGER_COUNT>>/$manager_count/g" |
+sed "s/<<TLD>>/$top_level_domain/g" |
 sed "s/<<ENCRYPTION_TOKEN>>/$encryption_token/g" > /out/server.config.json
 store_secret consul.server.config.json /out/server.config.json
 
 sed "s/<<REGION>>/$region/g" /app/agent.config.tmpl |
+sed "s/<<TLD>>/$top_level_domain/g" |
 sed "s/<<ENCRYPTION_TOKEN>>/$encryption_token/g" > /out/agent.config.json
 store_secret consul.agent.config.json /out/agent.config.json
