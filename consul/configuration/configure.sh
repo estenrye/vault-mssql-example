8d15d95e49c3c211ad0628215199b9665dc12736
Esten Rye <esten@ryezone.net>
2018-03-21 22:58:27 -0500

    Revert Configuration to base configuration with no SSL certs, no ACLs, and Gossip encryption between nodes.

--- consul/configuration/configure.sh
+++ consul/configuration/configure.sh
@@ -13,14 +13,21 @@ region=$(echo $REGION | sed 's/\//\\\//g')
 manager_count=$(echo $MANAGER_COUNT | sed 's/\//\\\//g')
 top_level_domain=$(echo $TLD | sed 's/\//\\\//g')
 encryption_token=$(echo $ENCRYPTION_TOKEN | sed 's/\//\\\//g')
-
+master_token=$(echo $MASTER_TOKEN | sed 's/\//\\\//g')
+if [[ -z $master_token ]]; then
+    master_token=$(uuidgen | sed 's/\//\\\//g')
+fi
+echo "Consul ACL Master Token: $master_token"
+echo "Consul ACL Agent Token: $agent_token"
 sed "s/<<REGION>>/$region/g" /app/server.config.tmpl |
 sed "s/<<MANAGER_COUNT>>/$manager_count/g" |
 sed "s/<<TLD>>/$top_level_domain/g" |
+sed "s/<<MASTER_TOKEN>>/$master_token/g" |
 sed "s/<<ENCRYPTION_TOKEN>>/$encryption_token/g" > /out/server.config.json
 store_secret consul.server.config.json /out/server.config.json
 
 sed "s/<<REGION>>/$region/g" /app/agent.config.tmpl |
 sed "s/<<TLD>>/$top_level_domain/g" |
+sed "s/<<MASTER_TOKEN>>/$master_token/g" |
 sed "s/<<ENCRYPTION_TOKEN>>/$encryption_token/g" > /out/agent.config.json
 store_secret consul.agent.config.json /out/agent.config.json
