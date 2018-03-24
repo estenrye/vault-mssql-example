4442bf0f4b8c8bf7d250e3cbd8ba2c2bf8362c61
Esten Rye <esten@ryezone.net>
2018-03-23 23:14:54 -0500

    Change tactic of inititialization.
    My original approach of building docker images to do the configuration had a
    flaw.  It did not account for the fact that ec2 instances apparently can't
    resolve route53 dns names.  To work around this, I am readding the ports
    for the server and agent containers.  This will allow me to communicate
    with the consul instances in a shell script using the bound port.
    
    This will requre me to place firewall rules to secure port 8500 from being
    accessed from outside the swarm.

--- consul/acl/acl.sh
+++ consul/acl/acl.sh
@@ -2,10 +2,6 @@ if [[ -z $MASTER_TOKEN ]]; then
     echo "MASTER_TOKEN environment variable cannot be empty.  Aborting."
     exit 1
 fi
-if [[ -z $CONSUL_URI ]]; then
-    echo "CONSUL_URI environment variable cannot be empty.  Aborting"
-    exit 1
-fi
 
 # Create Agent Token
 agentToken=$(curl --request PUT --header "X-Consul-Token: $MASTER_TOKEN" --data \
@@ -13,7 +9,7 @@ agentToken=$(curl --request PUT --header "X-Consul-Token: $MASTER_TOKEN" --data 
   "Name": "Agent Token",
   "Type": "client",
   "Rules": "node \"\" { policy = \"write\" } service \"\" { policy = \"read\" }"
-}' http://$CONSUL_URI/v1/acl/create)
+}' http://localhost:8500/v1/acl/create)
 
 # Extract Agent Token from the response.
 token=$(echo $agentToken | jq --raw-output ".ID")
@@ -22,7 +18,7 @@ token=$(echo $agentToken | jq --raw-output ".ID")
 curl --request PUT --header "X-Consul-Token: $MASTER_TOKEN" --data \
 '{
   "Token": "fe3b8d40-0ee0-8783-6cc2-ab1aa9bb16c1"
-}' http://$CONSUL_URI/v1/agent/token/acl_agent_token
+}' http://localhost:8500/v1/agent/token/acl_agent_token
 
 # Set the Anonymous Token Policy
 curl --request PUT --header "X-Consul-Token: $MASTER_TOKEN" --data \
@@ -30,10 +26,10 @@ curl --request PUT --header "X-Consul-Token: $MASTER_TOKEN" --data \
   "ID": "anonymous",
   "Type": "client",
   "Rules": "node \"\" { policy = \"read\" } service \"consul\" { policy = \"read\" } key \"\" { policy = \"deny\" }"
-}'  http://$CONSUL_URI/v1/acl/update
+}'  http://localhost:8500/v1/acl/update
 
 # Create the acl configuration
-sed "s/<<ACL_TOKEN>>/$region/g" /app/acl.json.tmpl > acl.json
+sed "s/<<ACL_TOKEN>>/$agentToken/g" /app/acl.json.tmpl > acl.json
 docker config create acl.json acl.json
 
 # Load configuration into services
