33d18cedc6741154a0349fb27a8b92a48828c429
Esten Rye <esten@ryezone.net>
2018-03-23 23:26:05 -0500

    add temporary password

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 sudo apk add util-linux
 docker network create -d overlay --subnet=192.168.0.0/16 default_net
 docker stack deploy -c ./traefik/traefik.stack.yml traefik
-MASTER_TOKEN=$(uuidgen)
+export MASTER_TOKEN='MyBigFluffyBunny'
 REGION='us-east-2'
 MANAGER_COUNT=3
 ENCRYPTION_TOKEN='rp8BG/IebnT1lkKfp9hDyQ=='
