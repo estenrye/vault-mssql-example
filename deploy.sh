533c31c2e2494282317f755cdffe0542aa2dc34a
Esten Rye <esten@ryezone.net>
2018-03-25 11:12:04 -0500

    add missing envronment var.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
-sudo apk add util-linux
-docker network create -d overlay --subnet=192.168.0.0/16 default_net
-docker stack deploy -c ./traefik/traefik.stack.yml traefik
 export MASTER_TOKEN='MyBigFluffyBunny'
 export REGION='us-east-2'
 export MANAGER_COUNT=3
 export ENCRYPTION_TOKEN='rp8BG/IebnT1lkKfp9hDyQ=='
+export TLD='d.ryezone.com'
+docker network create -d overlay --subnet=192.168.0.0/16 default_net
+docker stack deploy -c ./traefik/traefik.stack.yml traefik
+
 /bin/sh ./consul/configuration/configure.sh
 docker stack deploy -c ./consul/consul.stack.yml consul
 /bin/sh ./consul/acl/acl.sh
