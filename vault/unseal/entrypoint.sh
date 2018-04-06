15901c18e752db9e39e63a7ad9fec95300ec84be
Esten Rye <esten@ryezone.net>
2018-04-06 00:03:08 -0500

    Remove unnecessary tmp dir.  add unseal request.

--- vault/unseal/entrypoint.sh
+++ vault/unseal/entrypoint.sh
@@ -1,7 +1,6 @@
 keyIndex=$1
 passphrase=$2
 
-keysDir=$(mktemp -d)
 privatekey=$(curl --silent --request GET --header "X-Consul-Token: $CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/private_keys/pgp_keys/key$keyIndex.key?raw)
 ownertrust=$(curl --silent --request GET --header "X-Consul-Token: $CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/private_keys/pgp_keys/key$keyIndex.ownertrust.txt?raw)
 ciphertext=$(curl --silent --request GET --header "X-Consul-Token: $CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/seal_keys/key$index.cipher.txt?raw)
@@ -11,3 +10,5 @@ echo "$privatekey" | gpg --batch --passphrase $passphrase --import
 echo "$ownertrust" | gpg --batch --import-ownertrust
 
 seal_key=$(echo "$ciphertext" | gpg --batch --passphrase $passphrase --pinentry-mode loopback --decrypt)
+
+curl --request PUT --data "{ \"key\":\"$seal_key\"}" http://vault:8200/v1/sys/unseal
