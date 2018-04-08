1072668e5c5f6b51b0395d6fd71c57b27a351eea
Esten Rye <esten@ryezone.net>
2018-04-08 02:10:08 -0500

    fix uri bug in uri

--- vault/unseal/entrypoint.sh
+++ vault/unseal/entrypoint.sh
@@ -1,9 +1,9 @@
 keyIndex=$1
 passphrase=$2
 
-privatekey=$(curl --silent --request GET --header "X-Consul-Token: $CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/private_keys/pgp_keys/key$keyIndex.key?raw)
-ownertrust=$(curl --silent --request GET --header "X-Consul-Token: $CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/private_keys/pgp_keys/key$keyIndex.ownertrust.txt?raw)
-ciphertext=$(curl --silent --request GET --header "X-Consul-Token: $CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/seal_keys/key$index.cipher.txt?raw)
+privatekey=$(curl --request GET --header "X-Consul-Token: $CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/private_keys/pgp_keys/key$keyIndex.key?raw)
+ownertrust=$(curl --request GET --header "X-Consul-Token: $CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/private_keys/pgp_keys/key$keyIndex.ownertrust.txt?raw)
+ciphertext=$(curl --request GET --header "X-Consul-Token: $CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/seal_keys/key$keyIndex.cipher.txt?raw)
 
 export GNUPGHOME="$(mktemp -d)"
 echo "$privatekey" | gpg --batch --passphrase $passphrase --import
