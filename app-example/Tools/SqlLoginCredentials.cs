dda0afc7ad8dbf84937bde2ce1826d9569d8e3c9
Esten Rye <esten@ryezone.net>
2018-04-14 22:01:01 -0500

    Add example applicaiton to demonstrate vault functionality.

---
+++ app-example/Tools/SqlLoginCredentials.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tools
+{
+    public class SqlLoginCredentials
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ app-example/vault-example/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Tools;
+
+namespace vault_example.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public IConfiguration Configuration { get; set; }
+        // GET: Home
+        public ActionResult Index()
+        {
+            var model = new VaultAppRole(Configuration);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult GetSecrets([FromForm]VaultAppRole model)
+        {
+            model.GetSecrets();
+            return View("Index", model);
+        }
+
+        [HttpPost]
+        public ActionResult GetToken([FromForm]VaultAppRole model)
+        {
+            model.GetToken();
+            return View("Index", model);
+        }
+
+        [HttpPost]
+        public ActionResult GetSqlCredentials([FromForm]VaultAppRole model)
+        {
+            model.GetCredentials();
+            return View("Index", model);
+        }
+    }
+}
