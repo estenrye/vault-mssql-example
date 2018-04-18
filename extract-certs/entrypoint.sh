3988d73204eafb3d229cff354e7e26cff5bd1e5d
Esten Rye <esten@ryezone.net>
2018-04-18 15:59:31 -0500

    Fix path in cp.

--- extract-certs/entrypoint.sh
+++ extract-certs/entrypoint.sh
@@ -1,3 +1,3 @@
 tar xzvf /run/secrets/letsencrypt
 mkdir -p /target/certs
-cp letsencrypt/config/live/wildcard-$PRIVATE_HOSTED_ZONE/*.pem /target/certs
+cp -r letsencrypt/config/live/wildcard-$PRIVATE_HOSTED_ZONE /target/certs
