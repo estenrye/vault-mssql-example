d3cbed1759db9c41c4e11bcf8dc73741ea0c6b11
Esten Rye <esten@ryezone.net>
2018-04-20 21:09:01 -0500

    fix init script.

--- vault/init/entrypoint.sh
+++ vault/init/entrypoint.sh
@@ -10,6 +10,12 @@ then
     exit 1
 fi
 
+if [[-z $VAULT_URI ]]
+then
+    echo 'VAULT_URI cannot be empty.  Are you missing an environment variable?'
+    exit 1
+fi
+
 if [[ -f /consul/certs/chain.pem ]]
 then
     cp /consul/certs/chain.pem /usr/local/share/ca-certificates/chain.pem
@@ -35,16 +41,20 @@ else
 fi
 
 update-ca-certificates
-
-secret_shares=$(curl --key /consul/certs/privkey.pem --cert /consul/certs/cert.pem --silent --request GET --header "X-Consul-Token:$CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/secret_shares?raw)
-if [[ -z secret_shares ]]
+echo "Retrieving Secret Shares."
+secret_shares=$(curl --key /consul/certs/privkey.pem --cert /consul/certs/cert.pem --request GET --header "X-Consul-Token:$CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/secret_shares?raw)
+echo "Secret Shares: $secret_shares"
+if [[ -z $secret_shares ]]
 then
     echo 'secret_shares is invalid.'
     exit 1
 fi
 
-secret_threshold=$(curl --key /consul/certs/privkey.pem --cert /consul/certs/cert.pem --silent --request GET --header "X-Consul-Token:$CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/secret_threshold?raw)
-if [[ -z secret_shares ]]
+echo "Retrieving Secret Threshold."
+secret_threshold=$(curl --key /consul/certs/privkey.pem --cert /consul/certs/cert.pem --request GET --header "X-Consul-Token:$CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/secret_threshold?raw)
+echo "Secret Threshold: $secret_threshold"
+
+if [[ -z $secret_threshold ]]
 then
     echo 'secret_threshold is invalid.'
     exit 1
@@ -53,8 +63,9 @@ fi
 pgp_keys='['
 for i in $(seq 1 $secret_shares)
 do
-    key=$(curl --key /consul/certs/privkey.pem --cert /consul/certs/cert.pem --silent --request GET --header "X-Consul-Token:$CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/public_keys/pgp_keys/key$i.key?raw | tr -d '\n')
-    if [[ -z secret_shares ]]
+    echo "Retrieving Public Key $i of $secret_shares"
+    key=$(curl --key /consul/certs/privkey.pem --cert /consul/certs/cert.pem --request GET --header "X-Consul-Token:$CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/public_keys/pgp_keys/key$i.key?raw | tr -d '\n')
+    if [[ -z $key ]]
     then
         echo "Key index $i is invalid."
         exit 1
@@ -66,8 +77,9 @@ do
     fi
 done
 pgp_keys="$pgp_keys]"
-root_token_pgp_key=$(curl --key /consul/certs/privkey.pem --cert /consul/certs/cert.pem --silent --request GET --header "X-Consul-Token:$CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/public_keys/root_token_pgp_key/key0.key?raw | tr -d '\n')
-if [[ -z secret_shares ]]
+echo "Retrieving Root Token Public Key"
+root_token_pgp_key=$(curl --key /consul/certs/privkey.pem --cert /consul/certs/cert.pem --request GET --header "X-Consul-Token:$CONSUL_ACL_TOKEN" $CONSUL_URI/v1/kv/vault_keys/public_keys/root_token_pgp_key/key0.key?raw | tr -d '\n')
+if [[ -z $root_token_pgp_key ]]
 then
     echo 'root_token_pgp_key is invalid.'
     exit 1
@@ -83,17 +95,21 @@ initData=$(cat <<EOF
 EOF
 )
 
-initResponse=$(curl --key /consul/certs/privkey.pem --cert /consul/certs/cert.pem --request PUT --data "$initData" http://vault:8200/v1/sys/init)
-
+echo "Sending Vault initialization request."
+initResponse=$(curl --key /consul/certs/privkey.pem --cert /consul/certs/cert.pem --request PUT --data "$initData" $VAULT_URI/v1/sys/init)
+echo "Response received."
 keys_base64=$(echo "$initResponse" | jq --raw-output '.keys_base64 | .[]')
 root_token=$(echo "$initResponse" | jq --raw-output '.root_token')
 index=1
 for key in $keys_base64
 do
-  curl --key /consul/certs/privkey.pem --cert /consul/certs/cert.pem --silent --request PUT --header "X-Consul-Token: $CONSUL_ACL_TOKEN" --data "$key" $CONSUL_URI/v1/kv/vault_keys/seal_keys/key$index.base64.txt
+  echo "Writing secret share ciphertext $index of $secret_shares to consul."
+  curl --key /consul/certs/privkey.pem --cert /consul/certs/cert.pem --request PUT --header "X-Consul-Token: $CONSUL_ACL_TOKEN" --data "$key" $CONSUL_URI/v1/kv/vault_keys/seal_keys/key$index.base64.txt
   let index=$index+1
 done
 
-curl --key /consul/certs/privkey.pem --cert /consul/certs/cert.pem --silent --request PUT --header "X-Consul-Token: $CONSUL_ACL_TOKEN" --data "$root_token" $CONSUL_URI/v1/kv/vault_keys/root_token.cipher.txt
+echo "Writing root token ciphertext to consul"
+curl --key /consul/certs/privkey.pem --cert /consul/certs/cert.pem --request PUT --header "X-Consul-Token: $CONSUL_ACL_TOKEN" --data "$root_token" $CONSUL_URI/v1/kv/vault_keys/root_token.cipher.txt
 
+echo "Operation complete"
 #echo $initResponse
