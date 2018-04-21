ace3af966b6f17a5b6461adc5e9cb51a6ea8d1e1
Esten Rye <esten@ryezone.net>
2018-04-21 16:37:39 -0500

    add missing slash

--- vault/deploy-vault.sh
+++ vault/deploy-vault.sh
@@ -20,7 +20,7 @@ docker run -d --name vault \
     -e 'VAULT_REDIRECT_INTERFACE=eth0' \
     -e "VAULT_CLUSTER_ADDR=https://vault.${PRIVATE_HOSTED_ZONE}" \
     -e "VAULT_API_ADDR=https://$VAULT_SERVER:8200" \
-    -e "VAULT_ADDR=https://$VAULT_SERVER:8200"
+    -e "VAULT_ADDR=https://$VAULT_SERVER:8200" \
     -v /home/docker/vault:/config \
     -v /home/docker/consul/certs:/consul/certs \
     --cap-add IPC_LOCK \
